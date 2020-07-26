7d4ab01fb2557c30d784fd12f445db9a2b2425a0
Akachukwu Udeh <akaudeh@gmail.com>
2020-07-26 23:30:32 +0100

    applied some styling, js codes and html

--- app.js
+++ app.js
@@ -1,2 +1,4 @@
 let today = new Date();
 document.getElementById("currenttime").innerHTML = today.getHours() + ":" + today.getMinutes();
+document.getElementById("currenttime2").innerHTML = today.getHours() + ":" + today.getMinutes();
+document.getElementById("currenttime3").innerHTML = today.getHours() + ":" + today.getMinutes();
