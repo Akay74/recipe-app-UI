89e244595af8ddbedfc4b443bd9202d47ed67746
Akachukwu Udeh <akaudeh@gmail.com>
2020-07-28 00:17:58 +0100

    modified JavaScript file

--- app.js
+++ app.js
@@ -1,4 +1,14 @@
 let today = new Date();
-document.getElementById("currenttime").innerHTML = today.getHours() + ":" + today.getMinutes();
-document.getElementById("currenttime2").innerHTML = today.getHours() + ":" + today.getMinutes();
-document.getElementById("currenttime3").innerHTML = today.getHours() + ":" + today.getMinutes();
+let h = today.getHours();
+let m = today.getMinutes();
+m = checkTime(m);
+h = checkTime(h);
+
+document.getElementById("currenttime").innerHTML = h + ":" + m;
+document.getElementById("currenttime2").innerHTML = h + ":" + m;
+document.getElementById("currenttime3").innerHTML = h + ":" + m;
+
+function checkTime(i) {
+    if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+    return i;
+  }
